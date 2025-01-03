c794aa12e6ddca12f6e389ba34ffd24ef7b5ff9e
samsond <nosmaspoise@gmail.com>
2025-01-03 15:23:40 -0800

    Implement native function call to binary_search using Panama

--- abi-ffi-examples/java/demo-ffi/src/main/java/org/example/App.java
+++ abi-ffi-examples/java/demo-ffi/src/main/java/org/example/App.java
@@ -1,13 +1,58 @@
 package org.example;
 
+import java.lang.foreign.*;
+import java.lang.invoke.MethodHandle;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        String libraryPath = "../../rust/binary_search_lib/target/release/libbinary_search_lib.dylib";
+
+
+
+        try {
+            try (Arena arena = Arena.ofConfined()) {
+                // Load the library
+                SymbolLookup lib = SymbolLookup.libraryLookup(libraryPath, arena);
+
+                // Find the binary_search function
+                MemorySegment binarySearchSymbol = lib.find("binary_search").orElseThrow();
+
+                // Prepare the function descriptor
+                FunctionDescriptor descriptor = FunctionDescriptor.of(
+                        ValueLayout.JAVA_INT,   // return type
+                        ValueLayout.ADDRESS,    // arr type
+                        ValueLayout.JAVA_INT,   // len type
+                        ValueLayout.JAVA_INT    // target type
+                );
+
+                // Create a MethodHandle for calling the binary_search function
+                MethodHandle methodHandle = Linker.nativeLinker().downcallHandle(binarySearchSymbol, descriptor);
+
+                // Prepare arguments
+                int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+                int len = arr.length;
+                int target = 7;
+
+                // Allocate off-heap memory and copy the array data
+                MemorySegment arrSegment = arena.allocateArray(ValueLayout.JAVA_INT, arr);
+
+                // Call the binary_search function
+                int result = (int) methodHandle.invokeExact(arrSegment, len, target);
+
+                System.out.println("Result: " + result);
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+        } catch (UnsatisfiedLinkError e) {
+            System.err.println("Error loading library: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
     }
 }
